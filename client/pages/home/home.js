74c3960b8071f9cd411940449596b63bcd16d51a
AleksandrLXX <zjlinxiangxiang@qq.com>
2019-06-03 16:13:47 +0800

    增加首页

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,14 +1,29 @@
 let app = getApp()
 Page({
-  data: {},
+  data: {
+    org: {}
+  },
   onLoad() {
+    my.showLoading({
+      content: '正在加载...',
+    });
     this._getMenus()
   },
   _getMenus() {
+    basement.db.collection('orgs')
+      .findOne({ mac: app.globalData.mac })
+      .then(({ result: org }) => {
+          
+        console.log('org', org)
+        this.setData({ org });
+        my.hideLoading({
+        });
+      })
+      .catch(console.error);
     basement.db.collection('menus')
       .find({ mac: app.globalData.mac }, { sort: { uploadTime: -1 } })
       .then(({ result: menus }) => {
-        console.log('menus',menus)
+        console.log('menus', menus)
         menus.map((item) => {
           // item.uploadTime = new Date(item.uploadTime).toDateString();
           return item;
